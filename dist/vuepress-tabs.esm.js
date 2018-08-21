2f1182dfe8285364300c549cfc95cf3776a57ab2
Panagiotis Skordilakis <p.skordilakis@gmail.com>
2018-08-21 15:24:58 +0300

    add dist files

---
+++ dist/vuepress-tabs.esm.js
@@ -0,0 +1,43 @@
+var container = require('markdown-it-container');
+
+var tabs = (function (md) {
+  md.use(container, 'tabs', {
+    render: function render(tokens, idx) {
+      // return `<span>tabs goes here</span>`
+      var token = tokens[idx];
+
+      if (token.nesting === 1) {
+        return '<div class="vuepress-tabs"><tabs class="custome-tabs">\n';
+      } else {
+        return '</tabs></div>\n';
+      }
+    }
+  });
+});
+
+var container$1 = require('markdown-it-container');
+
+var tab = (function (md) {
+  md.use(container$1, 'tab', {
+    render: function render(tokens, idx) {
+      var token = tokens[idx];
+      var info = token.info.trim().slice('tab'.length).trim();
+
+      if (token.nesting === 1) {
+        return '<tab name="' + info + '">\n';
+      } else {
+        return '</tab>\n';
+      }
+    }
+  });
+});
+
+var index = (function (md) {
+  // register tabs container
+  tabs(md);
+
+  //register tab container
+  tab(md);
+});
+
+export default index;
